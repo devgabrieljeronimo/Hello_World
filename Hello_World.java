4dbecd06cdea159948e72295d211d4aa496408c4
Jerônimo <gabrieljeronimo2b.007@gmail.com>
2025-01-23 09:40:05 -0300

    Agradecimentos a guanabara
    Thank u

--- Hello_World.java
+++ Hello_World.java
@@ -1,6 +1,7 @@
 public class Hello_World {
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
+        System.out.println("Hello World!" +
+         "\n" + "Obrigado guanabara!");
     }
 }
