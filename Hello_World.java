e0a9b31d737b6aec7657d304acac55e2f184214b
Jerônimo <gabrieljeronimo2b.007@gmail.com>
2025-01-27 18:55:54 -0300

    Testing...

--- Hello_World.java
+++ Hello_World.java
@@ -1,7 +1,22 @@
+import java.util.ArrayList;
+
 public class Hello_World {
     public static void main(String[] args) {
 
         System.out.println("Hello World!" +
          "\n" + "Obrigado guanabara!");
+
+        int[] nums = new int[3];
+        nums[0] = 1;
+
+        for ( int i : nums) {
+            System.out.println(i);
+        }
+
+        ArrayList<Integer> nums2 = new ArrayList<>();
+
+        nums2.add(1);
+        nums2.add(2);
+        System.out.println(nums2.isEmpty());
     }
 }
