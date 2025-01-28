6269b3d88fc48542f50ae6b9ff59c64997cb2a27
Jerônimo <gabrieljeronimo2b.007@gmail.com>
2025-01-27 21:31:32 -0300

    I make a "Padlock"

---
+++ Arrays.java
@@ -0,0 +1,17 @@
+public class Arrays {
+    public static void main(String[] args) {
+
+        /*int[][] Padlock = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        for (int[] linha : Padlock) {
+            for (int valor : linha) {
+                System.out.print(valor + " ");
+            }
+            System.out.println();
+        }*/
+    }
+}
