a757ca8c67b7a17ec17b535f2e0bff2884722dde
Gabriel Jeronimo <gabrieljeronimo2b.007@gmail.com>
2025-06-12 07:45:26 -0300

    organized the Hello_World

--- Hello_World.java
+++ Hello_World.java
@@ -4,22 +4,6 @@ public class Hello_World {
     public static void main(String[] args) {
 
         System.out.println("Hello World!" +
-         "\n" + "Obrigado guanabara!");
-
-        int[] nums = new int[3];
-        nums[0] = 1;
-
-        for ( int i : nums) {
-            System.out.println(i);
-        }
-
-        ArrayList<Integer> nums2 = new ArrayList<>();
-
-        nums2.add(1);
-        nums2.add(2);
-        nums2.add(3);
-        nums2.add(4);
-        nums2.add(5);
-        System.out.println(nums2.isEmpty());
+         "\n" + "Thank you so much guanabara!");
     }
 }
